462c44dd373fb657166ef63d76c17391b71a0727
sergiogiurcaits <sergio.giurca@itstechnologygroup.com>
2025-05-07 14:17:08 +0100

    Update testjob.py

--- testjob.py
+++ testjob.py
@@ -3,7 +3,7 @@ import os
 
 def main(runtime):
     # Get the testscript path (update with your actual test script)
-    testscript = os.path.join(os.path.dirname(__file__), 'test_script.py')
+    testscript = os.path.join(os.path.dirname(__file__), 'testscript.py')
 
     # Optional: define testbed path if not passed via CLI or Xpresso
     testbed = runtime.testbed or os.path.join(os.path.dirname(__file__), 'testbed.yaml')
